624896060a03beb078bbd543154b5df0ef0be0c8
luxiaozuo <1115669126@qq.com>
2018-11-23 08:40:31 +0800

    渲染了右边的列表

---
+++ Desktop/vue/vue第五天/blackbuy/src/components/index.vue
@@ -0,0 +1,265 @@
+<template>
+    <div>
+        <div class="section">
+            <div class="location">
+                <span>当前位置：</span>
+                <a href="#/" class="router-link-active">首页</a>&gt;
+                <a
+                    href="#/site/goodslist"
+                    class="router-link-exact-active router-link-active"
+                >购物商城</a>
+            </div>
+        </div>
+        <div class="section">
+            <div class="wrapper">
+                <div class="wrap-box">
+                    <div class="left-220" style="margin: 0px;">
+                        <div class="banner-nav">
+                            <ul>
+                                <li v-for="(item, index) in catelist" :key="index">
+                                    <h3>
+                                        <i class="iconfont icon-arrow-right"></i>
+                                        <span>{{item.title}}</span>
+                                        <p>
+                                            <span v-for="(item, index) in item.subcates" :key="index">{{item.title}}&nbsp;</span>
+                                            <!-- <span>摄影摄像&nbsp;</span>
+                                            <span>存储设备&nbsp;</span> -->
+                                        </p>
+                                    </h3>
+                                    <div class="item-box">
+                                        <dl>
+                                            <dt>
+                                                <a href="/goods/40.html">{{item.title}}</a>
+                                            </dt>
+                                            <dd>
+                                                <a v-for="(item, index) in item.subcates" :key="index" href="/goods/43.html">{{item.title}}</a>
+                                               
+                                            </dd>
+                                        </dl>
+                                    </div>
+                                </li>
+                                <!-- <li>
+                                    <h3>
+                                        <i class="iconfont icon-arrow-right"></i>
+                                        <span>电脑办公</span>
+                                        <p>
+                                            <span>电脑整机&nbsp;</span>
+                                            <span>外设产品&nbsp;</span>
+                                            <span>办公打印&nbsp;</span>
+                                        </p>
+                                    </h3>
+                                    <div class="item-box">
+                                        <dl>
+                                            <dt>
+                                                <a href="/goods/40.html">电脑办公</a>
+                                            </dt>
+                                            <dd>
+                                                <a href="/goods/43.html">电脑整机</a>
+                                                <a href="/goods/43.html">外设产品</a>
+                                                <a href="/goods/43.html">办公打印</a>
+                                            </dd>
+                                        </dl>
+                                    </div>
+                                </li>
+                                <li>
+                                    <h3>
+                                        <i class="iconfont icon-arrow-right"></i>
+                                        <span>影音娱乐</span>
+                                        <p>
+                                            <span>平板电视&nbsp;</span>
+                                            <span>音响DVD&nbsp;</span>
+                                            <span>影音配件&nbsp;</span>
+                                        </p>
+                                    </h3>
+                                    <div class="item-box">
+                                        <dl>
+                                            <dt>
+                                                <a href="/goods/40.html">影音娱乐</a>
+                                            </dt>
+                                            <dd>
+                                                <a href="/goods/43.html">平板电视</a>
+                                                <a href="/goods/43.html">音响DVD</a>
+                                                <a href="/goods/43.html">影音配件</a>
+                                            </dd>
+                                        </dl>
+                                    </div>
+                                </li>
+                                <li>
+                                    <h3>
+                                        <i class="iconfont icon-arrow-right"></i>
+                                        <span>服装类</span>
+                                        <p>
+                                            <span>男装&nbsp;</span>
+                                            <span>女装&nbsp;</span>
+                                        </p>
+                                    </h3>
+                                    <div class="item-box">
+                                        <dl>
+                                            <dt>
+                                                <a href="/goods/40.html">服装类</a>
+                                            </dt>
+                                            <dd>
+                                                <a href="/goods/43.html">男装</a>
+                                                <a href="/goods/43.html">女装</a>
+                                            </dd>
+                                        </dl>
+                                    </div>
+                                </li> -->
+                            </ul>
+                        </div>
+                    </div>
+                    <!--幻灯片-->
+                    <div class="left-705">
+                        <div class="banner-img">
+                            <div id="focus-box" class="focus-box">
+                                <ul class="slides">
+                                    <li
+                                        class=""
+                                        style="width: 100%;height:100%; float: left; margin-right: -100%; position: relative; opacity: 0; display: block; z-index: 1;"
+                                    >
+                                        <a href="/goods.html">
+                                            <img
+                                                style="width: 100%;height:100%;"
+                                                src="http://39.108.135.214:8899/imgs/SJ4EgwosX0wTqvyAvhtFGT1w.jpg"
+                                                draggable="false"
+                                            >
+                                        </a>
+                                    </li>
+                                    <li
+                                        style="width: 100%;height:100%; float: left; margin-right: -100%; position: relative; opacity: 1; display: block; z-index: 2;"
+                                        class="flex-active-slide"
+                                    >
+                                        <a href="/goods.html">
+                                            <img
+                                                style="width: 100%;height:100%;"
+                                                src="http://39.108.135.214:8899/upload/201504/20/thumb_201504200314272543.jpg"
+                                                draggable="false"
+                                            >
+                                        </a>
+                                    </li>
+                                </ul>
+                                <ol class="flex-control-nav flex-control-paging">
+                                    <li>
+                                        <a class="">1</a>
+                                    </li>
+                                    <li>
+                                        <a class="flex-active">2</a>
+                                    </li>
+                                </ol>
+                            </div>
+                        </div>
+                    </div>
+                    <!--/幻灯片-->
+                    <div class="left-220">
+                        <ul class="side-img-list">
+                            <li v-for="(item, index) in toplist" :key="item.id">
+                                <div class="img-box">
+                                    <label>{{ index+1 }}</label>
+                                    <img
+                                        :src="item.img_url"
+                                    >
+                                </div>
+                                <div class="txt-box">
+                                    <a href="/goods/show-98.html">{{item.title}}</a>
+                                    <span>{{item.add_time | shorttime}}</span>
+                                </div>
+                            </li>
+                           
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="section" v-for="(item, index) in message" :key="index">
+          <div class="main-tit">
+                <h2>{{item.catetitle}}</h2>
+                <p>
+                    <a v-for="(item, index) in item.level2catelist" :key="index" href="/goods/43.html">{{item.subcatetitle}}</a>
+                    <a href="/goods/40.html">
+                        更多
+                        <i>+</i>
+                    </a>
+                </p>
+          </div>
+           <div class="wrapper clearfix">
+                <div class="wrap-box">
+                    <ul class="img-list">
+                        <li v-for="(item, index) in item.datas" :key="index">
+                            <a :href="'#/site/goodsinfo/'+item.artID" class="">
+                                <div class="img-box">
+                                    <img
+                                        :src="item.img_url"
+                                    >
+                                </div>
+                                <div class="info">
+                                    <h3>{{item.artTitle}}</h3>
+                                    <p class="price">
+                                        <b>{{item.sell_price}}</b>元
+                                    </p>
+                                    <p>
+                                        <strong>库存 {{item.stock_quantity}}</strong>
+                                        <span>
+                                            市场价：
+                                            <s>{{item.market_price}}</s>
+                                        </span>
+                                    </p>
+                                </div>
+                            </a>
+                        </li>
+                    
+                        
+                    </ul>
+                </div>
+           </div>
+        
+       </div>
+       
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+import moment from 'moment'
+export default {
+  name: "index",
+  data:function(){
+      return {
+        catelist:[],
+        sliderlist:[],
+        toplist:[],
+        message:[]
+       
+        
+      }
+  },
+  methods:{
+    
+  },
+  created() {
+      axios.get('http://111.230.232.110:8899/site/goods/gettopdata/goods')
+      .then(result=>{
+          console.log(result);
+        this.catelist = result.data.message.catelist;
+        this.sliderlist = result.data.message.sliderlist;
+        this.toplist = result.data.message.toplist;
+      })
+
+      axios.get('http://111.230.232.110:8899/site/goods/getgoodsgroup')
+      .then(result=>{
+        //   console.log(result);
+        this.message = result.data.message
+        // console.log(this.message);
+       
+      })
+  },
+  filters:{
+      shorttime(value){
+        // console.log(value);
+        return moment(value).format('YYYY年MM月DD日')
+      }
+  }
+};
+</script>
+
+<style>
+</style>
--- Desktop/vue/vue第五天/blackbuy/src/main.js
+++ Desktop/vue/vue第五天/blackbuy/src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import index from './components/index.vue'
+
+Vue.use(VueRouter)
+import './assets/site/css/style.css'
 
 Vue.config.productionTip = false
-
+let routes = [
+  {path:'/',redirect:'/index'},
+  {path:'/index',component:index}
+]
+const router = new VueRouter({
+  routes
+})
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
